a56a02367b41c8188c13742d29440df9b83aa931
zecarlos <>
2021-11-02 09:09:58 -0500

    C.F y C.D

---
+++ configServer/src/test/java/com/configServer/MsPolitics1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPolitics1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
